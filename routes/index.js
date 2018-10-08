b142e2cd4a04f76af3760d484774bb25e1ae42eb
jorgeality <jorgeality29@gmail.com>
2018-10-08 05:48:12 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,34 @@ var jsforce = require('jsforce');
 
 /* Send information to salesforce */
 function createContact(lastName, email) {
+  var res;
   var conn = new jsforce.Connection({
-      // you can change loginUrl to connect to sandbox or prerelease env.
-      loginUrl: 'https://test.salesforce.com'
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    loginUrl: 'https://test.salesforce.com'
   });
   /*you need to put here your user and your password to do login */
   conn.login('cgarrett@myhomes.services.preprod', 'Oce@ns11', function (err, res) {
-      if (err) { return console.error(err); }
-      conn.sobject("Contact").create({ LastName: lastName, Email: email }, function (err, ret) {
-          if (err || !ret.success) { return console.error(err, ret); }
-          console.log(ret);
-          // ...
-      });
+    if (err) { return console.error(err); }
+    conn.sobject("Contact").create({ LastName: lastName, Email: email }, function (err, ret) {
+      if (err || !ret.success) { return console.error(err, ret); }
+      console.log(ret);
+      res = ret;
+      // ...
+    });
   });
+  return res;
 }
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', description: 'this is a little example how to use the jsforce library to send information to salesforce org' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
+/*POST send information */
 app.post('/contacts', function (req, res, next) {
-  createContact(req.Email, req.Email)
+  var resp = createContact(req.Email, req.Email);
+  res.send(resp);
 })
 
 
