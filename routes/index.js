d6209e2700c38550c265e62516c37929ca16fdfb
jorgeality <jorgeality29@gmail.com>
2018-10-08 08:28:12 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,26 @@ router.use(bodyParser.json());
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express2' });
+  res.render('index', { title: 'Express' });
 });
 
 /*POST send information */
 router.post('/api/contacts', function (req, res) {
-  /*var conn = new jsforce.Connection({
+  var conn = new jsforce.Connection({
     // you can change loginUrl to connect to sandbox or prerelease env.
     loginUrl: 'https://test.salesforce.com'
   });
-  /*you need to put here your user and your password to do login 
+  /*you need to put here your user and your password to do login */
   conn.login('cgarrett@myhomes.services.preprod', 'Oce@ns11', function (err, res) {
     if (err) { return res.send(err); }
-    conn.sobject("Contact").create({ LastName: req.email, Email: req.email }, function (err, ret) {
+    conn.sobject("Contact").create({ LastName: req.body.email, Email: req.body.email }, function (err, ret) {
       if (err || !ret.success) { return console.error(err, ret); }
       console.log(ret);
       res.send(ret);
-      // ...
     });
   });
-*/
-  res.send('Got a POST request'+ req.body.email)
+
+  //res.send('Got a POST request'+ req.body.email)
 });
 
 
