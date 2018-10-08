d4f92dfb6fd9e6623b59c366e5b8192c12511cc7
jorgeality <jorgeality29@gmail.com>
2018-10-08 06:14:19 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var jsforce = require('jsforce');
+var bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }))
+router.use(bodyParser.json());
 
 
 /* GET home page. */
