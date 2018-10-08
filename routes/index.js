9546556ac05787e774d8b8f6732b155021369075
jorgeality <jorgeality29@gmail.com>
2018-10-08 06:00:49 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,6 @@ var express = require('express');
 var router = express.Router();
 var jsforce = require('jsforce');
 
-/* Send information to salesforce */
-function createContact(lastName, email) {
-  var res;
-  var conn = new jsforce.Connection({
-    // you can change loginUrl to connect to sandbox or prerelease env.
-    loginUrl: 'https://test.salesforce.com'
-  });
-  /*you need to put here your user and your password to do login */
-  conn.login('cgarrett@myhomes.services.preprod', 'Oce@ns11', function (err, res) {
-    if (err) { return console.error(err); }
-    conn.sobject("Contact").create({ LastName: lastName, Email: email }, function (err, ret) {
-      if (err || !ret.success) { return console.error(err, ret); }
-      console.log(ret);
-      res = ret;
-      // ...
-    });
-  });
-  return res;
-}
-
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -30,8 +10,21 @@ router.get('/', function (req, res, next) {
 
 /*POST send information */
 router.post('/contacts', function (req, res, next) {
-  var resp = createContact(req.Email, req.Email);
-  res.send(resp);
+  var conn = new jsforce.Connection({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    loginUrl: 'https://test.salesforce.com'
+  });
+  /*you need to put here your user and your password to do login */
+  conn.login('cgarrett@myhomes.services.preprod', 'Oce@ns11', function (err, res) {
+    if (err) { return res.send(err); }
+    conn.sobject("Contact").create({ LastName: req.email, Email: req.email }, function (err, ret) {
+      if (err || !ret.success) { return console.error(err, ret); }
+      console.log(ret);
+      res.send(ret);
+      // ...
+    });
+  });
+
 })
 
 
