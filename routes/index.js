b51329af64f0feb39a48c98c4ce2d8123d0dd255
jorgeality <jorgeality29@gmail.com>
2018-10-08 05:27:41 -0500

    .

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var jsforce = require('jsforce');
+
+/* Send information to salesforce */
+function createContact(lastName, email) {
+  var conn = new jsforce.Connection({
+      // you can change loginUrl to connect to sandbox or prerelease env.
+      loginUrl: 'https://test.salesforce.com'
+  });
+  /*you need to put here your user and your password to do login */
+  conn.login('cgarrett@myhomes.services.preprod', 'Oce@ns11', function (err, res) {
+      if (err) { return console.error(err); }
+      conn.sobject("Contact").create({ LastName: lastName, Email: email }, function (err, ret) {
+          if (err || !ret.success) { return console.error(err, ret); }
+          console.log(ret);
+          // ...
+      });
+  });
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +26,7 @@ router.get('/', function(req, res, next) {
 });
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'HOME' });
+  createContact('jorgeality@testDiacohome.co', 'jorgeality@testDiacohome.co')
 });
 
 module.exports = router;
